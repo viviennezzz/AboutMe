2b061a6721f86138fae1fbac8f9d4ed80bb7e2f5
viviennezzz <vivienne82394@gmail.com>
2017-02-05 14:28:53 -0500

    Add JS functions

---
+++ aboutMe.js
@@ -0,0 +1,6 @@
+function showAlert() {
+			alert("\"Train yourself to let go of everything you fear to lose.\"\n" 
+				+ "                                                                          - Yoda");
+		}
+
+
